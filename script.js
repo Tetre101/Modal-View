7ea8ee37e67262e03db974c8fba04e1f22e34ea9
Tetre101 <basilpeter989@gmail.com>
2023-01-28 08:05:20 +0100

    added the options of opening and closing modals

--- script.js
+++ script.js
@@ -4,7 +4,21 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
+
+const openModal = function () {
+  modal.classList.remove("hidden");
+  // modal.style.display = 'block'(would have done same thing as removing the hidden class)
+  overlay.classList.remove("hidden");
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener("click", openModal);
 }
+
+const closeModals = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModals);
+overlay.addEventListener("click", closeModals);
